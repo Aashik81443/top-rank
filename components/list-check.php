5370a2b65c6abfea48e99694911cc560ce26be72
yato <shreejann18@gmail.com>
2025-04-07 17:00:07 +0545

    ul-list-updated

--- components/list-check.php
+++ components/list-check.php
@@ -1,5 +1,10 @@
-<div class="list-check">
- <ul>
-  <li><i class="icon-tick"></i>LIst one</li>
- </ul>
+<div class="check">
+    <ul class="checkmark"> 
+         <li> <i class="icon-tick"></i>Foundation Repair</li>
+         <li><i class="icon-tick"></i>Chimney Repair</li>
+         <li><i class="icon-tick"></i>Basement Waterproofing</li>
+         <li><i class="icon-tick"></i>Stoops and Steps</li>
+         <li><i class="icon-tick"></i>Pations</li>
+         <li><i class="icon-tick"></i>Walkways</li>
+    </ul>
 </div>
