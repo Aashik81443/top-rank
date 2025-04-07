112128bc627500e1f0c75dc4a9755d8c0b4fc4bf
Krisha <krishashrestha2059@gmail.com>
2025-04-07 20:26:36 +0545

    media-offset

--- components/media-offset.php
+++ components/media-offset.php
@@ -1,3 +1,7 @@
 <div class="media-offset">
-
+    <div class="container">
+          <figure>
+            <img src="./assets/images/about-us.jpg" alt="About Us" width = "562.26px" height="393.58px" loading="lazy">
+          </figure>
+          </div>
 </div>
