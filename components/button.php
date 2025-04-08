2135fed660cd616a9e395129cbec4d421b1611c2
Aashik Sharma <sharma81443@gmail.com>
2025-04-08 11:22:40 +0545

    button and media offset reviewed

--- components/button.php
+++ components/button.php
@@ -1,4 +1,3 @@
 <a href="#" class="btn">Contact Us</a>
 <a href="#" class="btn btn-primary">Submit</a>
-<a href="#" class="btn btn-second">Contact Us</a> <hr>
-<a href="tel:(978)330-0758" class="btn btn-third">Get A Free Quote - Call(978)330-0758</a>
+<a href="tel:(978)330-0758" class="btn btn-primary">Get A Free Quote - Call(978)330-0758</a>
