3ca2609c96803f559e23ba6b32500bf705599b59
Aashik Sharma <sharma81443@gmail.com>
2025-04-08 15:23:39 +0545

    Services section updates

---
+++ components/card-service.php
@@ -0,0 +1,11 @@
+<div class="card-service">
+    <div class="media-offset">
+        <figure>
+            <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
+        </figure>
+    </div>
+    <div class="content">
+        <h3>Foundation Repair</h3>
+        <p>Our team provides expert foundation repair services across Massachusetts, ensuring the strength and stability of homes and buildings. We specialize in addressing foundation issues such as cracks, settling, and water damage, using high-quality materials and advanced techniques to restore structural integrity. Whether your foundation requires minor crack repairs or major reinforcement, we offer reliable and long-lasting solutions tailored to your property’s needs. With a strong commitment to excellence and customer satisfaction, Top Rank Masonry and Paving helps homeowners protect their investments and prevent costly future damage.</p>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include './header.php';
   <h3><em>Typography</em></h3>
   <h1>Heading one</h1>
   <h2>Heading one</h2>
-  <h3>Heading one</h3> 
+  <h3>Heading one</h3>
   <hr>
   <h3><em>Body Copy</em></h3>
   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga aliquid molestias ex labore praesentium, esse quisquam consequuntur eveniet eligendi, nostrum quis magni eaque iure. Consequuntur.</p>
@@ -15,6 +15,77 @@ include './header.php';
   <?php
   include './components.php';
   ?>
+ </div>
+</section>
+
+<section class="block-services">
+ <div class="container">
+  <header class="block-title">
+   <div class="aboutus-text">
+    <h2>Our Services</h2>
+    <p>The Top Rank team is equipped to handle all of your masonry needs:</p>
+   </div>
+  </header>
+  <div class="card-wrapper">
+   <div class="row">
+    <div class="col">
+     <div class="card-service">
+      <div class="media-offset">
+       <figure>
+        <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
+       </figure>
+      </div>
+      <div class="content">
+       <h3>Foundation Repair</h3>
+       <p>Our team provides expert foundation repair services across Massachusetts, ensuring the strength and stability of homes and buildings. We specialize in addressing foundation issues such as cracks, settling, and water damage, using high-quality materials and advanced techniques to restore structural integrity. Whether your foundation requires minor crack repairs or major reinforcement, we offer reliable and long-lasting solutions tailored to your property’s needs. With a strong commitment to excellence and customer satisfaction, Top Rank Masonry and Paving helps homeowners protect their investments and prevent costly future damage.</p>
+      </div>
+     </div>
+    </div>
+    <!--item-->
+    <div class="col">
+     <div class="card-service">
+      <div class="media-offset">
+       <figure>
+        <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
+       </figure>
+      </div>
+      <div class="content">
+       <h3>Foundation Repair</h3>
+       <p>Our team provides expert foundation repair services across Massachusetts, ensuring the strength and stability of homes and buildings. We specialize in addressing foundation issues such as cracks, settling, and water damage, using high-quality materials and advanced techniques to restore structural integrity. Whether your foundation requires minor crack repairs or major reinforcement, we offer reliable and long-lasting solutions tailored to your property’s needs. With a strong commitment to excellence and customer satisfaction, Top Rank Masonry and Paving helps homeowners protect their investments and prevent costly future damage.</p>
+      </div>
+     </div>
+    </div>
+    <!--item-->
+    <div class="col">
+     <div class="card-service">
+      <div class="media-offset">
+       <figure>
+        <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
+       </figure>
+      </div>
+      <div class="content">
+       <h3>Foundation Repair</h3>
+       <p>Our team provides expert foundation repair services across Massachusetts, ensuring the strength and stability of homes and buildings. We specialize in addressing foundation issues such as cracks, settling, and water damage, using high-quality materials and advanced techniques to restore structural integrity. Whether your foundation requires minor crack repairs or major reinforcement, we offer reliable and long-lasting solutions tailored to your property’s needs. With a strong commitment to excellence and customer satisfaction, Top Rank Masonry and Paving helps homeowners protect their investments and prevent costly future damage.</p>
+      </div>
+     </div>
+    </div>
+    <!--item-->
+    <div class="col">
+     <div class="card-service">
+      <div class="media-offset">
+       <figure>
+        <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
+       </figure>
+      </div>
+      <div class="content">
+       <h3>Foundation Repair</h3>
+       <p>Our team provides expert foundation repair services across Massachusetts, ensuring the strength and stability of homes and buildings. We specialize in addressing foundation issues such as cracks, settling, and water damage, using high-quality materials and advanced techniques to restore structural integrity. Whether your foundation requires minor crack repairs or major reinforcement, we offer reliable and long-lasting solutions tailored to your property’s needs. With a strong commitment to excellence and customer satisfaction, Top Rank Masonry and Paving helps homeowners protect their investments and prevent costly future damage.</p>
+      </div>
+     </div>
+    </div>
+    <!--item-->
+   </div>
+  </div>
  </div>
 </section>
 
