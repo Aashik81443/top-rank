e402cc450bcda54dc3213f6634c9345c4270dca2
yato <shreejann18@gmail.com>
2025-04-08 10:17:09 +0545

    buttonsupdated

--- components/button.php
+++ components/button.php
@@ -1,2 +1,4 @@
 <a href="#" class="btn">Contact Us</a>
 <a href="#" class="btn btn-primary">Submit</a>
+<a href="#" class="btn btn-second">Contact Us</a> <hr>
+<a href="tel:(978)330-0758" class="btn btn-third">Get A Free Quote - Call(978)330-0758</a>
