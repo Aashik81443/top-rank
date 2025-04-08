f1f2e1103acfcc4052141e3d696c89c8eaf7810b
yato <shreejann18@gmail.com>
2025-04-08 11:57:41 +0545

    about-us-block-title

--- components/block-title.php
+++ components/block-title.php
@@ -1,3 +1,7 @@
 <header class="block-title">
- Block title
+<div class="aboutus-text">
+     <h3>ABOUT US</h3>
+     <h2>YOUR TRUSTED CONTRACTOR FOR HIGH-QUALITY MASORNY SERVICES.</h2>
+     <p>From upgrading your outdoor space to making crucial repairs to your home, our team is dedicated to providing durable craftsmanship with exceptional customer service. All of our projects include a free estimate and a 10-year guarantee. Reach out today to get started!</p><br>
+     </div>
 </header>
