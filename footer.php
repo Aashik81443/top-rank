2d1420bab95b30549e81f1e6ec301d5103e7b922
Aashik Sharma <sharma81443@gmail.com>
2025-04-08 15:07:07 +0545

    Header and footer reviewed

--- footer.php
+++ footer.php
@@ -1,11 +1,13 @@
-<footer class="footer">
-    <div class="footer-section">
-        <a href="#"><i class ="icon-call-alt"></i>(978) 330-0758</a>
+<footer class="site-footer">
+    <div class="container">
+        <div class="phone-number">
+            <a href="tel:9783300758"><i class="icon-call-alt"></i>(978) 330-0758</a>
+        </div>
     </div>
 </footer>
 
 <script src="./vendors/jquery.min.js"></script>
- <script src="./assets/js/main.js"></script>
+<script src="./assets/js/main.js"></script>
 </body>
 
 </html>
--- header.php
+++ header.php
@@ -2,31 +2,31 @@
 <html lang="en">
 
 <head>
- <meta charset="UTF-8">
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
- <link rel="preconnect" href="https://fonts.googleapis.com">
- <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
- <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
-  rel="stylesheet">
- <link rel="stylesheet" href="./style.css">
- <title>Top Rank</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="./style.css">
+    <title>Top Rank</title>
 </head>
 
 <body>
 
     <!--Header Section Starting-->
-    <header class="site-header"> 
-        <div class="container"> 
-            <div class="contents">
-                 <div class="header-logo">
-                     <figure>
-                         <img src="./assets/images/logo.png" alt="Header Logo" width="63px" height="40px" loading="lazy">
-                     </figure>
-                 </div>
+    <header class="site-header">
+        <div class="container">
+            <div class="content">
+                <div class="header-logo">
+                    <figure>
+                        <img src="./assets/images/logo.png" alt="Header Logo" width="63px" height="40px" loading="lazy">
+                    </figure>
+                </div>
                 <div class="phone-number">
-                    <a href="tel(978)330-0758"><i class="icon-call"></i>(978) 330-0758</a>
+                    <a href="tel:9783300758"><i class="icon-call"></i><span class="number">(978) 330-0758</span></a>
                 </div>
             </div>
-        </div>        
+        </div>
     </header>
-        <!--Header Section Ending--> 
+    <!--Header Section Ending-->
