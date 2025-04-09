1715ebe6fe067ea6f768529278f1baa2b0f592c0
Gajendra Sah <sahgajendra121@gmail.com>
2025-04-09 11:18:02 +0545

    get-in-touch section and our-services section styling updated

--- index.php
+++ index.php
@@ -3,124 +3,175 @@ include './header.php';
 ?>
 
 <section class="block-components">
- <div class="container">
-  <h3><em>Typography</em></h3>
-  <h1>Heading one</h1>
-  <h2>Heading one</h2>
-  <h3>Heading one</h3>
-  <hr>
-  <h3><em>Body Copy</em></h3>
-  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga aliquid molestias ex labore praesentium, esse quisquam consequuntur eveniet eligendi, nostrum quis magni eaque iure. Consequuntur.</p>
-  <hr>
-  <?php
-  include './components.php';
-  ?>
- </div>
+    <div class="container">
+        <h3><em>Typography</em></h3>
+        <h1>Heading one</h1>
+        <h2>Heading one</h2>
+        <h3>Heading one</h3>
+        <hr>
+        <h3><em>Body Copy</em></h3>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fuga aliquid molestias ex labore praesentium, esse quisquam consequuntur eveniet eligendi, nostrum quis magni eaque iure. Consequuntur.</p>
+        <hr>
+        <?php
+        include './components.php';
+        ?>
+    </div>
 </section>
 
 <section class="block-about">
- <div class="container">
-  <div class="row">
-   <div class="col-left">
-    <div class="media-offset">
-     <figure>
-      <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
-     </figure>
+    <div class="container">
+        <div class="row">
+            <div class="col-left">
+                <div class="media-offset">
+                    <figure>
+                        <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
+                    </figure>
+                </div>
+            </div>
+            <div class="col-right">
+                <header class="block-title">
+                    <span class="eyebrow">About Us</span>
+                    <h2>Your trusted contractor for high-quality masonry services.</h2>
+                    <p>From upgrading your outdoor space to making crucial repairs to your home, our team is dedicated to providing durable craftsmanship with exceptional customer service. All of our projects include a free estimate and a 10-year guarantee. Reach out today to get started!
+                    </p>
+                </header>
+                <div class="btn-wrap">
+                    <a href="#" class="btn btn-primary">Contact Us</a>
+                </div>
+            </div>
+        </div>
     </div>
-   </div>
-   <div class="col-right">
-    <header class="block-title">
-     <span class="eyebrow">About Us</span>
-     <h2>Your trusted contractor for high-quality masonry services.</h2>
-     <p>From upgrading your outdoor space to making crucial repairs to your home, our team is dedicated to providing durable craftsmanship with exceptional customer service. All of our projects include a free estimate and a 10-year guarantee. Reach out today to get started!
-     </p>
-    </header>
-    <div class="btn-wrap">
-     <a href="#" class="btn btn-primary">Contact Us</a>
-    </div>
-   </div>
-  </div>
- </div>
 </section>
 
 <section class="block-services">
- <div class="container">
-  <header class="block-title text-white text-center">
-   <h2>Our Services</h2>
-   <p>The Top Rank team is equipped to handle all of your masonry needs:</p>
-  </header>
-  <div class="card-wrapper">
-   <div class="row">
-    <div class="col">
-     <div class="card-service">
-      <div class="media-offset">
-       <figure>
-        <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
-       </figure>
-      </div>
-      <div class="content">
-       <h3>Foundation Repair</h3>
-       <p>Our team provides expert foundation repair services across Massachusetts, ensuring the strength and stability of homes and buildings. We specialize in addressing foundation issues such as cracks, settling, and water damage, using high-quality materials and advanced techniques to restore structural integrity. Whether your foundation requires minor crack repairs or major reinforcement, we offer reliable and long-lasting solutions tailored to your property’s needs. With a strong commitment to excellence and customer satisfaction, Top Rank Masonry and Paving helps homeowners protect their investments and prevent costly future damage.</p>
-      </div>
-     </div>
+    <div class="container">
+        <header class="block-title text-white text-center">
+            <h2>Our Services</h2>
+            <p>The Top Rank team is equipped to handle all of your masonry needs:</p>
+        </header>
+        <div class="card-wrapper">
+            <div class="row">
+                <div class="col">
+                    <div class="card-service">
+                        <div class="media-offset">
+                            <figure>
+                                <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
+                            </figure>
+                        </div>
+                        <div class="content">
+                            <h3>Foundation Repair</h3>
+                            <p>Our team provides expert foundation repair services across Massachusetts, ensuring the strength and stability of homes and buildings. We specialize in addressing foundation issues such as cracks, settling, and water damage, using high-quality materials and advanced techniques to restore structural integrity. Whether your foundation requires minor crack repairs or major reinforcement, we offer reliable and long-lasting solutions tailored to your property’s needs. With a strong commitment to excellence and customer satisfaction, Top Rank Masonry and Paving helps homeowners protect their investments and prevent costly future damage.</p>
+                        </div>
+                    </div>
+                </div>
+                <!--item-->
+                <div class="col">
+                    <div class="card-service">
+                        <div class="media-offset">
+                            <figure>
+                                <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
+                            </figure>
+                        </div>
+                        <div class="content">
+                            <h3>Chimney Repair</h3>
+                            <p>At Top Rank Masonry, we take pride in keeping your chimney safe, functional, and looking its best. Serving homeowners across Massachusetts, our experienced team specializes in repairing cracks, brick deterioration, mortar damage, and leaks using top-quality materials to restore structural integrity. Whether you need a minor fix or a complete chimney rebuild, we provide reliable, long-lasting solutions tailored to your needs. Committed to excellence and customer satisfaction, we help you protect your home, prevent costly damage, and extend the life of your chimney.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <!--item-->
+                <div class="col">
+                    <div class="card-service">
+                        <div class="media-offset">
+                            <figure>
+                                <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
+                            </figure>
+                        </div>
+                        <div class="content">
+                            <h3>Foundation Repair</h3>
+                            <p>Our team provides expert foundation repair services across Massachusetts, ensuring the strength and stability of homes and buildings. We specialize in addressing foundation issues such as cracks, settling, and water damage, using high-quality materials and advanced techniques to restore structural integrity. Whether your foundation requires minor crack repairs or major reinforcement, we offer reliable and long-lasting solutions tailored to your property’s needs. With a strong commitment to excellence and customer satisfaction, Top Rank Masonry and Paving helps homeowners protect their investments and prevent costly future damage.</p>
+                        </div>
+                    </div>
+                </div>
+                <!--item-->
+                <div class="col">
+                    <div class="card-service">
+                        <div class="media-offset">
+                            <figure>
+                                <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
+                            </figure>
+                        </div>
+                        <div class="content">
+                            <h3>Foundation Repair</h3>
+                            <p>Our team provides expert foundation repair services across Massachusetts, ensuring the strength and stability of homes and buildings. We specialize in addressing foundation issues such as cracks, settling, and water damage, using high-quality materials and advanced techniques to restore structural integrity. Whether your foundation requires minor crack repairs or major reinforcement, we offer reliable and long-lasting solutions tailored to your property’s needs. With a strong commitment to excellence and customer satisfaction, Top Rank Masonry and Paving helps homeowners protect their investments and prevent costly future damage.</p>
+                        </div>
+                    </div>
+                </div>
+                <!--item-->
+            </div>
+        </div>
     </div>
-    <!--item-->
-    <div class="col">
-     <div class="card-service">
-      <div class="media-offset">
-       <figure>
-        <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
-       </figure>
-      </div>
-      <div class="content">
-       <h3>Foundation Repair</h3>
-       <p>Our team provides expert foundation repair services across Massachusetts, ensuring the strength and stability of homes and buildings. We specialize in addressing foundation issues such as cracks, settling, and water damage, using high-quality materials and advanced techniques to restore structural integrity. Whether your foundation requires minor crack repairs or major reinforcement, we offer reliable and long-lasting solutions tailored to your property’s needs. With a strong commitment to excellence and customer satisfaction, Top Rank Masonry and Paving helps homeowners protect their investments and prevent costly future damage.</p>
-      </div>
-     </div>
-    </div>
-    <!--item-->
-    <div class="col">
-     <div class="card-service">
-      <div class="media-offset">
-       <figure>
-        <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
-       </figure>
-      </div>
-      <div class="content">
-       <h3>Foundation Repair</h3>
-       <p>Our team provides expert foundation repair services across Massachusetts, ensuring the strength and stability of homes and buildings. We specialize in addressing foundation issues such as cracks, settling, and water damage, using high-quality materials and advanced techniques to restore structural integrity. Whether your foundation requires minor crack repairs or major reinforcement, we offer reliable and long-lasting solutions tailored to your property’s needs. With a strong commitment to excellence and customer satisfaction, Top Rank Masonry and Paving helps homeowners protect their investments and prevent costly future damage.</p>
-      </div>
-     </div>
-    </div>
-    <!--item-->
-    <div class="col">
-     <div class="card-service">
-      <div class="media-offset">
-       <figure>
-        <img src="./assets/images/about-us.jpg" alt="About Us" width="562.26px" height="393.58px" loading="lazy">
-       </figure>
-      </div>
-      <div class="content">
-       <h3>Foundation Repair</h3>
-       <p>Our team provides expert foundation repair services across Massachusetts, ensuring the strength and stability of homes and buildings. We specialize in addressing foundation issues such as cracks, settling, and water damage, using high-quality materials and advanced techniques to restore structural integrity. Whether your foundation requires minor crack repairs or major reinforcement, we offer reliable and long-lasting solutions tailored to your property’s needs. With a strong commitment to excellence and customer satisfaction, Top Rank Masonry and Paving helps homeowners protect their investments and prevent costly future damage.</p>
-      </div>
-     </div>
-    </div>
-    <!--item-->
-   </div>
-  </div>
- </div>
 </section>
 
 <section class="block-contact">
- <div class="container">
-  <header class="block-title text-center">
-   <span class="eyebrow">Get in Touch</span>
-   <h2>Contact Us Today & Get Your Free Quote</h2>
-   <p>Tell us about yourself and share some details of your upcoming project. We can’t wait to get started!</p>
-  </header>
+    <div class="container">
+        <header class="block-title text-center">
+            <span class="eyebrow">Get in Touch</span>
+            <h2>Contact Us Today & Get Your Free Quote</h2>
+            <p>Tell us about yourself and share some details of your upcoming project. We can’t wait to get started!</p>
+        </header>
 
- </div>
+        <div class="row no-gutter">
+            <div class="col col-lg-6">
+                <div class="block-contact__media">
+                    <img src="./assets/images/map.png" alt="Map Image" width="708" height="522" loading="lazy">
+                </div>
+            </div>
+            <div class="col col-lg-6">
+                <div class="block-contact__form" id="contactForm">
+                    <form method="" action="" id="form-id-0" class="validate form">
+                        <div class="form-section-0">
+                            <div class="form-group">
+                                <label for="name">Name</label>
+                                <input class="form-control" placeholder="Name" id="name" name="name" type="text" value="">
+                                <div class="messages"></div>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input class="form-control" placeholder="Email Address" id="email" name="email" type="email" value="">
+                                <div class="messages"></div>
+                            </div>
+                            <div class="form-group">
+                                <label for="phoneNumber">Phone Number</label>
+                                <input class="form-control" placeholder="Phone Number" id="phoneNumber" name="phone-number" type="text" value="">
+                                <div class="messages"></div>
+                            </div>
+                            <div class="form-group">
+                                <label for="projectLocation">Project Location</label>
+                                <input class="form-control" placeholder="Project Location" id="projectLocation" name="projectLocation" type="text" value="">
+                                <div class="messages"></div>
+                            </div>
+                            <div class="form-group">
+                                <textarea class="form-control" placeholder="Message..." name="message" cols="50" rows="10"></textarea>
+                                <div class="messages"></div>
+                            </div>
+                        </div>
+
+                        <div class="form-footer">
+                            <button type="submit" class="submitButton btn btn-primary btn-block">
+                                <svg class="animate-spin -ml-1 mr-3 h-4 w-4 text-white loader" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" aria-hidden="true" style="display: none">
+                                    <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                    <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                </svg>
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </section>
 
 <?php
